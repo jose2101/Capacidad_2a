165fb37f5b59951d287e61c0554e60c89c6fadc3
Jose <jose.21.01.chavez@gmail.com>
2022-05-30 20:50:01 -0500

    Subida 1 Chavez Gamarra

---
+++ gestion_archivos.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 16:40:36 2022
+
+@author: Carlos
+"""
+
+import os
+def crear_archivo(nombre,contenido):
+        archivo = open(nombre,"at")
+        archivo.write(contenido)
+        archivo.close()
+
+
