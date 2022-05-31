cb90840d25e54d4a08fe98072c359fc16525436e
Jose <jose.21.01.chavez@gmail.com>
2022-05-30 21:33:22 -0500

    Subida 5(menu) Chavez Gamarra

---
+++ aplicacion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 17:29:02 2022
+
+@author: Carlos
+"""
+
+import gestion_archivos
+
+def menu():
+    print("*MENU PRINCIPAL*")
+    print("1. Crear Archivo")
+    print("2. Eliminar archivo")
+    print("3. Agregar contenido")
+    print("4. Mostrar contenido del archivo")
+    print("5.Salir")
